d9f06c10ca6b0f38bb313f10017e1ea22be9e9b0
Jhon Lopez <jhonlopez1121@gmail.com>
2022-06-27 15:07:07 -0500

    añadiendo responsive y funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+// loader
 window.onload = () => {
     let contenedor = document.querySelector(".loader_container");
 
     contenedor.style.display = "none";
 }
+// menu Mobile
+
+let menuMobile = document.querySelector(".menuMobile");
+let menuList = document.querySelector(".menuLinks");
+let menuOptions = document.querySelectorAll(".menuOptions");
+
+menuMobile.addEventListener("click", () => {
+    menuMobile.classList.toggle("active");
+    menuList.classList.toggle("active");
+});
+
+menuOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        menuMobile.classList.remove("active");
+        menuList.classList.remove("active");
+    })
+});
+// modo oscuro/claro
 
 let btnSwitch = document.querySelector(".switch");
 
@@ -32,3 +51,5 @@ if (localStorage.getItem('lightMode') === 'true') {
     btnSwitch.classList.remove('active');
     document.body.classList.add('dark');
 }
+
+// validacion formulario
