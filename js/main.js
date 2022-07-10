830ef9e45d3b2ae20ee77c000af2ebf30ebb46b2
Jhon Lopez <jhonlopez1121@gmail.com>
2022-07-09 23:40:01 -0500

    arreglando responsive formulario

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // loader
 window.onload = () => {
-    let contenedor = document.querySelector(".loader_container");
+    let container = document.querySelector(".loader_container");
 
-    contenedor.style.display = "none";
+    container.style.display = "none";
 }
 
 // boton de ir hacia arriba
 
 let btnUp = document.querySelector("#botonUp");
 
-window.onscroll = ()=>{
+window.onscroll = () => {
 
     let scroll = document.documentElement.scrollTop;
 
     if (scroll > 100) {
         btnUp.style.transform = "scale(1)"
-    }else{
+    } else {
         btnUp.style.transform = "scale(0)"
     }
 }
@@ -71,17 +71,40 @@ if (localStorage.getItem('lightMode') === 'true') {
 // validacion formulario
 
 const form = document.querySelector('.contactoForm');
-const formEmail = document.querySelector("#email");
-const validateRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const inputs = document.querySelectorAll("#formulario input");
 const formButton = document.getElementsByClassName("button");
 
-form.addEventListener('submit', (evento) => {
-    evento.preventDefault();
+const expresiones = {
+    user: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    subject: /^[A-Za-z0-9\s]+$/g, // Letras y espacios, pueden llevar acentos.
+}
 
-    if (validateRegEx.test(formEmail.value)) {
-        alert("correo valido")
-    } else {
-        alert("correo invalido")
+const validateForm = (event) => {
+    switch (event.target.name) {
+        case 'nombre':
+        console.log("nombree");
+        break;
+        case 'email':
+            console.log("emaillll");
+
+        break;
+        case 'asunto':
+            console.log("asuntoo");
+
+        break;
+
+        default:
+            break;
     }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 })
 
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validateForm);
+    input.addEventListener("blur", validateForm);
+})
